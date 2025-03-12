dbcbd3eaf4bc940682cfc7176a83551f8c7c64a9
Ana <148962937+mrkskq@users.noreply.github.com>
2025-03-12 20:04:51 +0100

    .

--- IT/labs/lab1_grupaA/Glasaj.aspx.designer.cs
+++ IT/labs/lab1_grupaA/Glasaj.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Lab1A
     {
 
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
         /// lblProfesor control.
         /// </summary>
         /// <remarks>
