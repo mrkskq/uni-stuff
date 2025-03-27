8886c33c38d5f2d0e26ebbffa8d0257c86800095
Ana <148962937+mrkskq@users.noreply.github.com>
2025-03-28 00:16:14 +0100

    .

---
+++ IT/audss/aud_4/zadaca_1/aud4zad1.aspx.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Primer
+{
+	public partial class aud4zad1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void register_Click(object sender, EventArgs e)
+        {
+			lb_reg.Text = "Uspeshno glasanje!";
+        }
+    }
+}
