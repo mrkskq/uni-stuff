65c6f6528e30069423495e5487d9769b2bd316cf
Ana <148962937+mrkskq@users.noreply.github.com>
2025-04-26 18:09:59 +0200

    .

---
+++ IT/pred/pred_8/Models/Album.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+	public class Album
+	{
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+    }
+}
