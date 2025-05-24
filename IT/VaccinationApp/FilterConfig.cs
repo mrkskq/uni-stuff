87612dc1ff47f002b21b10d53f0c20e9b50cbddc
Ana <148962937+mrkskq@users.noreply.github.com>
2025-05-24 18:04:03 +0200

    .

---
+++ IT/VaccinationApp/FilterConfig.cs
@@ -0,0 +1,14 @@
+using System.Web;
+using System.Web.Mvc;
+
+namespace VaccinationApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new AuthorizeAttribute());
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
