3232f771a0cf931c6ef34b754ca4b128f6c80a73
Ana <148962937+mrkskq@users.noreply.github.com>
2025-05-25 16:34:17 +0200

    .

---
+++ OS/networking/kolokviumskaLab/Worker.java
@@ -0,0 +1,87 @@
+package kolokviumskaLab;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Worker extends Thread {
+
+    private Socket socket;
+    private static int fileSize = 0;
+    private static Lock lock = new ReentrantLock();
+
+    public Worker(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        String index;
+        String filePath;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            String line;
+
+            line = reader.readLine(); //procitaj hello:index
+            index = line.split(":")[1]; //zemi go indeksot
+
+            if (!line.equalsIgnoreCase("hello:" + index)){
+                socket.close();
+            }
+
+            writer.write(index + ":hello\n");
+            writer.flush();
+
+            line = reader.readLine(); //procitaj index:attach:filename.txt
+            filePath = line.split(":")[2]; //zemi go filePath
+
+            if (!line.equalsIgnoreCase(index + ":attach:" + filePath)){
+                socket.close();
+            }
+
+            while (!(line = reader.readLine()).equalsIgnoreCase(index + ":over")) {
+                lock.lock();
+                int lineSize = line.getBytes().length;
+                fileSize += (lineSize + 1); // +1 za \n zs ne go brojt inace
+                lock.unlock();
+            }
+
+            writer.write("Sodrzinata na fajlot e uspeshno primena\n");
+            writer.flush();
+            writer.write(index + ":fileSize:" + fileSize + "\n");
+            writer.flush();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
